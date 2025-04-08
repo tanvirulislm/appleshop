0833731af22e18ba80c232d1951fd4d03ce586a9
Tanvirul <ownbinodon@gmail.com>
2025-04-08 19:18:56 +0600

    Started a e-commerce project

---
+++ database/migrations/2025_04_08_102523_create_invoices_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->string('total');
+            $table->string('vat');
+            $table->string('payable');
+            $table->string('cus_details');
+            $table->string('ship_details');
+            $table->string('tran_id');
+            $table->string('val_id');
+            $table->enum('delivery_status', ['Pending', 'Processing', 'Completed']);
+            $table->string('payment_status');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnUpdate()->restrictOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
